d6428546340f2c9578cc36b2d187c74202d72938
Portmeister <portmeister@gmail.com>
2021-01-16 20:45:27 -0600

    add db seed values

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name, devour) VALUES ('Patty Melt', true);
+INSERT INTO burgers (name, devour) VALUES ('Big Kahuna Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('WhataBurger', true);
+INSERT INTO burgers (name) VALUES ('Big Mac');
